488edfa6918f5e25157a6412ae5fb6ff7360d13a
boburbekburkhonov <zafaroviccc@gmail.com>
2023-01-11 18:36:35 +0500

    feat: port

--- src/server.js
+++ src/server.js
@@ -6,18 +6,20 @@ import routes from './module/routes.js';
 
 dotenv.config()
 
+const PORT = process.env.PORT || 9090;
+
 const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(routes)
-app.use(errorMiddleware)
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+app.use(errorMiddleware);
 
-app.all('/*', (req, res) => {
+app.all("/*", (req, res) => {
   res.status(404).json({
-    message: req.url + ' not found',
-    status: 404
-  })
-})
+    message: req.url + " not found",
+    status: 404,
+  });
+});
 
-app.listen(9090, console.log(9090))
+app.listen(PORT, console.log(PORT));
